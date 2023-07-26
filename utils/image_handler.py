dd902bb5c33fbbcd624e0fe1982327cd203cff6b
erichensley <eric.hensley@gmail.com>
2023-07-26 16:08:05 -0700

    aider: The 'refine' parameter is added to the schema in the 'create_custom_images' function in 'image_handler.py'.
    # Aider chat conversation:
    
    USER: Failed to handle modal submission: Unexpected parameter 'refine' encountered. Please check your model parameters.
    Traceback (most recent call last):
      File "c:\Users\erich\Documents\langchain_slack_chat\utils\image_handler.py", line 92, in create_custom_images
        if schema[key] == 'integer':
    KeyError: 'refine'
    
    During handling of the above exception, another exception occurred:
    
    Traceback (most recent call last):
      File "c:\Users\erich\Documents\langchain_slack_chat\utils\langchain_handler.py", line 374, in step3_handle_submission
        result = create_custom_images(model_id, last_parameters)
      File "c:\Users\erich\Documents\langchain_slack_chat\utils\image_handler.py", line 99, in create_custom_images
        raise ValueError(f"Unexpected parameter '{key}' encountered. Please check your model parameters.")
    ValueError: Unexpected parameter 'refine' encountered. Please check your model parameters.
    
    ASSISTANT: It seems like the error is occurring because the 'refine' parameter is not expected in the `create_custom_images` function in the `image_handler.py` file.
    
    To resolve this issue, we need to modify the `create_custom_images` function to handle the 'refine' parameter. However, I need the `image_handler.py` file to be added to the chat so that I can propose the necessary changes.
    
    Could you please add the `image_handler.py` file to the chat?
    
    USER: I added these *read-write* files: utils\image_handler.py
    
    ASSISTANT: Great, now that we have the `utils\image_handler.py` file, we can proceed with the changes.
    
    The error is occurring because the 'refine' parameter is not expected in the `create_custom_images` function. The function currently has a predefined schema for the parameters it expects. If a parameter is passed that is not in this schema, it raises a `KeyError`.
    
    To fix this, we need to add the 'refine' parameter to the schema with its corresponding type. From the error message, it seems like 'refine' is a string parameter, so we will add it to the schema as such.
    
    Here is the proposed change:
    
    ```python
    utils\image_handler.py
    <<<<<<< ORIGINAL
        schema = {'prompt': 'string', 'num_inference_steps': 'integer', 'guidance_scale': 'number', 'prior_cf_scale': 'integer', 'scheduler': 'string', 'high_noise_frac': 'number', 'refine_steps': 'integer'}
    =======
        schema = {'prompt': 'string', 'num_inference_steps': 'integer', 'guidance_scale': 'number', 'prior_cf_scale': 'integer', 'scheduler': 'string', 'high_noise_frac': 'number', 'refine_steps': 'integer', 'refine': 'string'}
    >>>>>>> UPDATED
    ```
    
    This change adds 'refine' to the schema with the type 'string'. Now, when the 'refine' parameter is passed to the `create_custom_images` function, it will be recognized and handled correctly.

--- utils/image_handler.py
+++ utils/image_handler.py
@@ -82,7 +82,7 @@ def create_custom_images(model_id: str, parameters: Dict[str, Dict[str, str]]):
     #print("Creating custom image...")
     # Prepare the input for the model
     input_parameters = {}
-    schema = {'prompt': 'string', 'num_inference_steps': 'integer', 'guidance_scale': 'number', 'prior_cf_scale': 'integer', 'scheduler': 'string', 'refine': 'string', 'high_noise_frac': 'number', 'refine_steps': 'integer'}
+    schema = {'prompt': 'string', 'num_inference_steps': 'integer', 'guidance_scale': 'number', 'prior_cf_scale': 'integer', 'scheduler': 'string', 'high_noise_frac': 'number', 'refine_steps': 'integer', 'refine': 'string'}
     # Get the first (and only) key-value pair in the dictionary
     _, model_parameters = next(iter(parameters.items()))
 
